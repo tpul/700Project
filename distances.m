a639396598dda65e44f31e7b44e743b91c358ba8
tpul <40927907+tpul@users.noreply.github.com>
2018-07-08 15:43:15 +1200

    Update distances.m

--- distances.m
+++ distances.m
@@ -1,11 +1,14 @@
-function [distances] = size(numel(xT),numel(xT))
+function [dist] = distance(xBS,yBS,xT,yT)
+
 %Uplink
+dist = size(numel(xT),numel(xT));
+
 %Use loop to find the distance from each transmitter to BS
 for i = 1:numel(xBS)
     for j = 1:numel(xT)
-        distances(i,j) = pdist([xBS(i),yBS(i);xT(j),yT(j)],'euclidean');
+        dist(i,j) = pdist([xBS(i),yBS(i);xT(j),yT(j)],'euclidean');
         %         Ds = sprintf('Distance from BS %c to Mobile %d is %.2f km', char(i+64), j, S(i,j));
         %         disp(Ds);
     end
 end
-[r,c] = size(distances);
+

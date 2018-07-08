aa4800401dfe633f509c85509d1dc3248707dd56
tpul <40927907+tpul@users.noreply.github.com>
2018-07-08 19:02:45 +1200

    Update PathLoss.m

--- PathLoss.m
+++ PathLoss.m
@@ -1,11 +1,12 @@
 function [gain] = PathLoss(distances)
 %Calculate the gain(path loss) of each Tx to each BS
+
+[r,c] = size(distances);
 gain = [r,c];
+
 for i = 1:r
     for j = 1:c
         gain(i,j) = (1/(distances(i,j)^4));
     end
 end
 
-SIR = [r,c];
-Pairs = NaN(r,c);

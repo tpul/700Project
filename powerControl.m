ffd412c12360f280c56619d0e0b1e0dea882d9df
tpul <40927907+tpul@users.noreply.github.com>
2018-07-08 19:30:27 +1200

    Update powerControl.m

--- powerControl.m
+++ powerControl.m
@@ -4,7 +4,7 @@ function [pairs] = powerControl(matrixOfPairs, numberOfBS,numberOfMobiles)
 %balance the powers between all Tx with respect to median number.
 
 %retrieves the median number in the matrix
-% m = round(median(matrixOfPairs(:),'omitnan'));
+
 
 midRangeValue = round((max(matrixOfPairs(:))+ min(matrixOfPairs(:)))/2);
 

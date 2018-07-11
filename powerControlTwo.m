0a3b62d4c2b2404d4431095e8f5a7ecfd9feac56
tpul <40927907+tpul@users.noreply.github.com>
2018-07-11 15:55:16 +1200

    Add files via upload

---
+++ powerControlTwo.m
@@ -0,0 +1,37 @@
+function [balancedSIR] = powerControlTwo(gain, pair)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+[r,c] = size(gain);
+
+pairDiff = sum(diff(sort(pair(~isnan(pair))')));
+newPairDiff = 0;
+count = 0;
+
+[max_num,max_num_ind] = max(gain(:));
+[X,Y] = ind2sub(size(gain),max_num_ind);
+
+while ( newPairDiff < pairDiff)
+    for i = 1:r
+        for j = 1:c
+            if i == X && j == Y
+                gain(i,j) = gain(i,j) - (gain(i,j)/4);
+            elseif j == Y
+                gain(i,j) = gain(i,j) - (gain(i,j)/4);
+            end
+        end
+    end
+    newSIR = signalToNoiseRatio(gain);
+    balancedPair = pairUp(newSIR);
+    newPairDiff = sum(diff(sort(balancedPair(~isnan(balancedPair))')));
+    if newPairDiff <= pairDiff
+        pairDiff = newPairDiff;
+        newPairDiff = 0;
+        balancedSIR = balancedPair;
+        count = count + 1;
+    elseif count == 0
+        balancedSIR = pair;
+    end
+end
+
+end
+
